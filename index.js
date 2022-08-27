c9beefb18a781aec6665218e042ca2733df6ff46
dan <dan.debiazi@gmail.com>
2022-08-27 15:18:10 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
